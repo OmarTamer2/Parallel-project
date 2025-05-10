dcccc9b3c64baaae3d157c170ba74ae3bf807a0a
OmarTamer2 <167299851+OmarTamer2@users.noreply.github.com>
2025-05-10 10:14:47 +0300

    adding new apis

--- signup.js
+++ signup.js
@@ -20,6 +20,23 @@ function validateEmail(email) {
 const app = express();
 app.use(express.json());
 
+const extractUserIdFromToken = async (token) => {
+    try {
+        const verifyResponse = await axios.post(
+            `http://${process.env.AUTH_HOST}:${process.env.AUTH_PORT}/auth/verify-token`,
+            { token }
+        );
+
+        if (!verifyResponse.data?.valid) {
+            throw new Error('Invalid token');
+        }
+
+        return verifyResponse.data.user; // Return the user ID
+    } catch (error) {
+        throw error;
+    }
+};
+
 app.post('/account/signup', async (req, res) => {
     try {
         const { name, email, password } = req.body;
@@ -55,7 +72,13 @@ app.post('/account/signup', async (req, res) => {
         await sequelize.query('INSERT INTO ACCOUNT(name, email, password_hash, balance) VALUES($1, $2, $3, $4)', {
             bind: [name, email, hashed_password, 0], // account_id is auto-incremented by the database whether Postgres or SQLite
         });
+
+        const record = await pool.query('select account_id from ACCOUNT where email = $1', [email]);
+        const account_id = record.rows[0].account_id;
+        await pool.query('insert into ACCOUNT_PREFERENCES(account_id) VALUES($1)', [account_id]);
+        await pool.query('insert into ACCOUNT_METADATA(account_id) VALUES($1)', [account_id]);
         return res.status(201).json('user inserted succesful');
+
     } catch (error) {
         return res.status(404).json({ Error: error.message });
     }
@@ -82,6 +105,8 @@ app.post('/account/login', async (req, res) => {
         // contact auth service and pass user id
         // noinspection HttpUrlsUsage
         const result = await axios.post(`http://${process.env.AUTH_HOST}:${process.env.AUTH_PORT}/auth/create-token?user_id=${results[0].account_id}`)
+
+        await sequelize.query('update ACCOUNT_METADATA set last_login = $1 where account_id = $2', [new Date(), result[0].account_id])
 
         //give token
         return res.status(200).json({...result.data, user: {email, name: results[0].name}});
@@ -111,7 +136,8 @@ app.post('/account/create-external-token', async (req, res) => {
 
             //give token
             return res.status(200).json(result.data);
-        } else {
+        } 
+        else {
             return res.status(401).json({ message: 'Unauthorized: Invalid token' });
         }
     } catch (error) {
@@ -119,6 +145,59 @@ app.post('/account/create-external-token', async (req, res) => {
         return res.status(500).json({ message: 'Internal server error: Authentication failed' });
     }
 });
+
+app.post('/account/preferences', async (req, res) =>{
+    const authHeader = req.headers.authorization;
+
+    //I feel like you can delete but just in case 
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Unauthorized: Missing or invalid Bearer token' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+
+        const user_id = extractUserIdFromToken(token);
+        const preferences = await sequelize.query('select * from ACCOUNT_PREFERENCES where account_id = $1', [user_id]);
+
+        if(preferences.length === 0){
+            return res.status(404).json({error: "No preferences found"})
+        }
+        return res.status(200).json(preferences)
+    
+    }
+    catch(err){
+        console.error('Error communicating with auth service:', error.message);
+        return res.status(500).json({ message: 'Internal server error: Authentication failed' });
+    }
+})
+
+app.put('/account/preferences', async (req, res) =>{
+    const {language, theme, notifications_enabled} = req.body;
+    const authHeader = req.headers.authorization;
+
+    //I feel like you can delete but just in case 
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Unauthorized: Missing or invalid Bearer token' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+
+        const user_id = extractUserIdFromToken(token);
+        await sequelize.query('update ACCOUNT_PREFERENCES put language = $1, theme = $2, notifications_enabled = $3 where account_id = $4',
+            [language, theme, notifications_enabled, user_id]
+        )
+        return res.status(200).json('Preferences updated')
+    
+    }
+    catch(err){
+        console.error('Error communicating with auth service:', error.message);
+        return res.status(500).json({ message: 'Internal server error: Authentication failed' });
+    }
+})
 
 const server = app.listen(PORT, () => {
     console.log("Listening on port " + PORT);
